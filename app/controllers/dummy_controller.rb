0383d2bbdf55464ad25157569655eab5e5e03a86
Nicholas Cummins <nicholas.r.cummins@gmail.com>
2014-03-02 15:13:53 -0500

    first commit

---
+++ app/controllers/dummy_controller.rb
@@ -0,0 +1,10 @@
+class DummyController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Devisetest::Application.config.secret_key_base = '274d1c8be0e1a71e6fbe9831b093c5e917acdd1fb415c7082e65b2283f0e00239d2529460ab7b46809472b47ca4dfea293eb64b6df9e062ba8bcd33bd329ae66'
